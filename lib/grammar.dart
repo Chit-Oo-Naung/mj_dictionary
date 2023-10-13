fb9d21a93a4215ee1bd8e91c0ea344376e175956
ChitOo-Naung <chit.on@spectrumsoft-tech.com>
2023-10-13 15:33:49 +0630

    add sample desigin in grammar

--- lib/grammar.dart
+++ lib/grammar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:mjdictionary/components/colors.dart';
 import 'package:mjdictionary/components/gradient_text.dart';
@@ -11,6 +12,9 @@ class GrammarPage extends StatefulWidget {
 }
 
 class _GrammarPageState extends State<GrammarPage> {
+
+  String htmlData = "<h3><span style='font-size:14px'>​​​​​​<span style='color:#c0392b'><strong>～んです。（～လို့ပါ）「အကြောင်းပြချက်၊ အလေးအနက်ပြောတဲ့နေရာမှာသုံး」</strong></span></span></h3><ul><li><span style='font-size:14px'><strong>Verb/いAdj　 ➞　PlainForm + <span style='color:#c0392b'>んです</span></strong></span></li><li><span style='font-size:14px'><strong>なAdj/Noun　➞　PlainForm <span style='color:#c0392b'>だ</span> ဖြုတ် + <span style='color:#c0392b'>んです</span></strong></span></li><li><span style='font-size:14px'><strong>1) ～んですか。　2) ～んですが、</strong></span><br />&nbsp;</li></ul><ol><li><span style='font-size:14px'>どうしておくれたんですか。</span><br />ဘာလို့နောက်ကျတာလဲ။<br />バスがこながったんです。<br />ဘတ်စ်ကားမလာလို့ပါ။</li><li>どうしたんですか。<br />ဘာဖြစ်လို့လဲ။<br />ちょっときぶんがわるいんです。<br />နေလို့သိပ်မကောင်းလို့ပါ။<br />&nbsp;</li></ol><p><span style='font-size:14px'><span style='color:#c0392b'><strong>Vて + いただけませんか。(Vပေးလို့ရနိုင်မလား)</strong></span></span><br /><span style='color:#c0392b'><span style='font-size:16px'><strong>​​​</strong></span></span></p><ol><li><span style='font-size:14px'>いいせんせいをしょうかいしていただけませんか。<br />ဆရာကောင်းကောင်းလေးမိတ်ဆက်ပေးလို့ရမလား။</span><br />&nbsp;</li></ol><p><span style='font-size:14px'>​​​​​​​​​​​​​​​​​​​​​</span><span style='color:#c0392b'><span style='font-size:16px'><strong><span style='font-size:14px'>Vた + らいいですか。(ရင်ကောင်းမလဲ) 「အကြံအဉာဏ်တောင်းတဲ့နေရာမှာသုံး」</span></strong></span></span></p><ol><li><span style='font-size:14px'><span style='color:#c0392b'>​​​​​​​</span>どこでカメラをかったらいいですか。<br />ဘယ်မှာကင်မရာကိုဝယ်ရင်ကောင်းမလဲ။</span></li><li><span style='font-size:14px'>こまがいおかねが、どうしたらいいですか。<br />ပိုက်ဆံအကြွေမရှိလို့ပါ, ဘယ်လိုလုပ်ရင်ကောင်းမလဲ။</span></li></ol>";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,18 +70,14 @@ class _GrammarPageState extends State<GrammarPage> {
                     ],
                   ),
                 ),
+                Expanded(
+                  child: Padding(
+                      padding:
+                          const EdgeInsets.only(top: 40.0, left: 0, right: 0),
+                      child: Html(data: htmlData)),
+                )
               ],
             ),
-            // Expanded(
-            //   child: Padding(
-            //     padding: const EdgeInsets.only(top: 40.0, left: 0, right: 0),
-            //     child: Column(
-            //       children: [
-                    
-            //       ],
-            //     ),
-            //   ),
-            // )
           ],
         ));
   }
